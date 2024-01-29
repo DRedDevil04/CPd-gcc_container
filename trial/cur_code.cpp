9bd040a08f7daebbe4f4cb85a97d9fabc28db1be
DRedDevil04 <devamdesai@gmail.com>
2024-01-29 17:25:56 +0530

    added docker in trial

--- trial/cur_code.cpp
+++ trial/cur_code.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main(){
-    printf("\nHello World, Devam!\n");
+    printf("\nHello World, Devam is GOAT\n");
     return 0;
 }
--- trial/trial.js
+++ trial/trial.js
@@ -1,21 +1,45 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 const { exec } = require('child_process'); 
- 
-// Compile the C program 
-exec('gcc -o hello hello_world.c', (error, stdout, stderr) => { 
-  if (error) { 
-    console.error(`exec error: ${error}`); 
-    return; 
-  } 
-  console.log(`stdout: ${stdout}`); 
-  console.error(`stderr: ${stderr}`); 
- 
-  // Execute the program 
-  exec('./hello', (error, stdout, stderr) => { 
-    if (error) { 
-      console.error(`exec error: ${error}`); 
-      return; 
-    } 
-    console.log(`stdout: ${stdout}`); 
-    console.error(`stderr: ${stderr}`); 
-  }); 
-}); 
+var fileupload = require("express-fileupload");
+app.use(fileupload());
+app.post('/upload', (req, res) => {
+    var file;
+    if(!req.files){
+        res.status(403).send("No file uploaded");
+
+    }
+    file = req.files.myFile;
+    file.mv('./cur_code.c', err => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        
+        
+    });
+    exec(`gcc -o hello ./cur_code.c`, (error, stdout, stderr) => { 
+        if (error) { 
+          console.error(`exec error: ${error}`); 
+          return; 
+        } 
+        console.log(`stdout: ${stdout}`); 
+        console.error(`stderr: ${stderr}`); 
+       
+        // Execute the program 
+        exec('./hello', (error, stdout, stderr) => { 
+          if (error) { 
+            console.error(`exec error: ${error}`); 
+            return; 
+          } 
+          console.log(`stdout: ${stdout}`); 
+          console.error(`stderr: ${stderr}`); 
+          res.send(stdout);
+        }); 
+    }); 
+   
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
