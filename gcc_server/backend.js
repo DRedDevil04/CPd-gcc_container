b8001cacc79390e2137cca88cc539dc8ac61f8f3
DRedDevil04 <devamdesai@gmail.com>
2024-01-29 15:40:09 +0530

    added primitive server code execution

---
+++ gcc_server/backend.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const { exec } = require('child_process'); 
+var fileupload = require("express-fileupload");
+app.use(fileupload());
+app.post('/upload', (req, res) => {
+    var file;
+    if(!req.files){
+        res.status(403).send("No file uploaded");
+
+    }
+    file = req.files.myFile;
+    file.mv('./code/cur_code.cpp', err => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        
+        
+    });
+    exec(`gcc -o hello ./code/cur_code.cpp`, (error, stdout, stderr) => { 
+        if (error) { 
+          console.error(`exec error: ${error}`); 
+          return; 
+        } 
+        console.log(`stdout: ${stdout}`); 
+        console.error(`stderr: ${stderr}`); 
+       
+        // Execute the program 
+        exec('./hello', (error, stdout, stderr) => { 
+          if (error) { 
+            console.error(`exec error: ${error}`); 
+            return; 
+          } 
+          console.log(`stdout: ${stdout}`); 
+          console.error(`stderr: ${stderr}`); 
+          res.send(stdout);
+        }); 
+    }); 
+   
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ gcc_server/code/cur_code.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(){
+    printf("\nHello World, Devam is GOAT\n");
+    return 0;
+}
