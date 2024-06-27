bf16af703c7413ac381c21d4244ce6aeee5a8b94
DRedDevil04 <devamdesai@gmail.com>
2024-06-27 12:59:25 +0530

    delete upload after compile error

--- gcc_server/backend.js
+++ gcc_server/backend.js
@@ -15,47 +15,53 @@ app.post("/upload", upload.any(), (req, res) => {
   const file = req.files[0];
   const testIP = req.files[1];
   const testOP = req.body.output;
-  console.log("New Request")
+  console.log("New Request");
   exec(
     `mv ./${file.path} ./uploads/${file.filename}.cpp`,
     (error, stdout, stderr) => {
-      exec(`g++-12 -o ./${file.path} ./${file.path}.cpp`, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`exec error: ${error}`);
-          return res.status(200).send({
-            success: false,
-            opstatus: "compile",
-            error: stderr,
-          });
-        }
-        exec(
-          `timeout 3s ./${file.path} < ./${testIP.path}`,
-          async (error, stdout, stderr) => {
+      exec(
+        `g++-12 -o ./${file.path} ./${file.path}.cpp`,
+        (error, stdout, stderr) => {
+          if (error) {
+            console.error(`exec error: ${error}`);
             deleteUpload(`./${file.path}.cpp`);
             deleteUpload(`./${testIP.path}`);
             deleteUpload(`./${file.path}`);
-            if (error) {
-              console.error(`exec error: ${error}`);
-              return res.status(200).send({
-                success: false,
-                opstatus: "run",
-                error: error,
-              });
-            }
-            if (validateFile(testOP, stdout)) {
-              res.status(200).send({
-                success: true,
-                opstatus: "accepted",
-              });
-            } else {
-              res.status(200).send({
-                success: true,
-                opstatus: "wrong",
-              });
-            }
+            return res.status(200).send({
+              success: false,
+              opstatus: "compile",
+              error: stderr,
+            });
           }
-        );
-      });
+          exec(
+            `timeout 3s ./${file.path} < ./${testIP.path}`,
+            async (error, stdout, stderr) => {
+              deleteUpload(`./${file.path}.cpp`);
+              deleteUpload(`./${testIP.path}`);
+              deleteUpload(`./${file.path}`);
+              if (error) {
+                console.error(`exec error: ${error}`);
+                return res.status(200).send({
+                  success: false,
+                  opstatus: "run",
+                  error: error,
+                });
+              }
+              if (validateFile(testOP, stdout)) {
+                res.status(200).send({
+                  success: true,
+                  opstatus: "accepted",
+                });
+              } else {
+                res.status(200).send({
+                  success: true,
+                  opstatus: "wrong",
+                });
+              }
+            }
+          );
+        }
+      );
     }
   );
 });
